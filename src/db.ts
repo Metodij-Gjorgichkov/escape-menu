a1f3c825b84094ef08c94a2b141247ed163f9cb9
Metodij-Gorgickov-FE11 <metodijgjorgichkov@yahoo.com>
2024-07-13 13:25:34 +0200

    initialized the project

---
+++ src/db.ts
@@ -0,0 +1,30 @@
+// export const contact: ContactType = {
+//       title: "Contact me",
+//       header: "Let's work together",
+//       info: [
+//         {
+//           name: "metodijgjorgichkov@yahoo.com",
+//           image: "/icons/email.png",
+//           linkTo: "mailto:metodijgjorgichkov@yahoo.com",
+//         },
+//         {
+//           name: "+41 77 472 54 51",
+//           image: "/icons/phone.png",
+//           linkTo: "tel:+41 77 472 54 51",
+//         },
+//         {
+//           name: "Based in Switzerland",
+//           image: "/icons/location.png",
+//         },
+//         {
+//           name: "Linkedin profile",
+//           image: "/icons/linkedin.png",
+//           linkTo: "https://www.linkedin.com/in/metodij-gjorgichkov-1074b9254",
+//         },
+//         {
+//           name: "Github profile",
+//           image: "/icons/github1.png",
+//           linkTo: "https://github.com/Metodij-Gjorgichkov",
+//         },
+//       ],
+//     };
---
+++ src/types/types.ts
@@ -0,0 +1,17 @@
+// export interface HeaderType {
+//       logo_image: string;
+//       logo_name: string;
+//       nav_links: {
+//         title: string;
+//         href: string;
+//       }[];
+//     }
+//     export interface BannerType {
+//       who_am_i: string;
+//       name: string;
+//       front_end: string;
+//       contact: string;
+//       contactLinkTo: string;
+//       image: string;
+//       passionate_frond_end: string;
+//     }
