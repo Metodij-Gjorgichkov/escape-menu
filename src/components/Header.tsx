bccf40a7fbef7c2c257dcb5452acdeebfbbeb339
Filip Gjorgichkov <gjorgichkov.filip@gmail.com>
2025-04-17 15:41:02 +0200

    made the cursor be a pointer when hovering over the language in the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,24 +5,33 @@ const Header = () => {
   const { lang, handleLanguageChange } = useContext(LangContext);
   return (
     <>
-    <div className="container position-relative">
+      <div className="container position-relative">
+        <div className="pos-absolute d-flex justify-content-between align-items-center main-color cursor-pointer">
+          <p
+            className={`${lang === "MK" ? "border-bottom-custom" : ""}`}
+            onClick={() => {
+              handleLanguageChange("MK");
+            }}
+          >
+            MK
+          </p>
+          <p className="mx-1">|</p>
+          <p
+            className={`${lang === "EN" ? "border-bottom-custom" : ""} `}
+            onClick={() => {
+              handleLanguageChange("EN");
+            }}
+          >
+            EN
+          </p>
+        </div>
 
-      <div className="pos-absolute d-flex justify-content-between align-items-center main-color">
-        <p className={`${lang === "MK" ? 'border-bottom-custom' : ''}`} onClick={() => {
-          handleLanguageChange("MK")
-        }}>MK</p>
-        <p className="mx-1">|</p>
-        <p className={`${lang === "EN" ? 'border-bottom-custom' : ''} `} onClick={() => {
-          handleLanguageChange("EN")
-        }}>EN</p>
-      </div>
-
-      <div className="row">
-        <div className="col-12 text-center">
-          <img className="logo" src="/images/logo.jpg" alt="logo" />  
+        <div className="row">
+          <div className="col-12 text-center">
+            <img className="logo" src="/images/logo.jpg" alt="logo" />
+          </div>
         </div>
       </div>
-     </div>
     </>
   );
 };
